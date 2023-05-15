000380df8350dcb4e928c946795d497e03766a4e
krukd <krukd87@gmail.com>
2023-05-15 10:47:01 +0200

    Добавьте файлы проекта.

---
+++ SkillFactory-Module-8.6-part2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillFactory_Module_8._6_part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
